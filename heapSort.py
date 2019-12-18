1dbadace9da6998e2e0728628a84d32cbb4174ad
jacpyr <anandharaj90@gmail.com>
2019-12-19 00:30:46 +0530

    adding Heap Sort

---
+++ heapSort.py
@@ -0,0 +1,44 @@
+
+def parent(i):
+    return (i-1)//2
+
+def left(i):
+    return 2*i+1
+
+def right(i):
+    return 2*i+2
+
+def maxHeapify(A,i,heapSize):
+    l = left(i)
+    r = right(i)
+    largest = i
+    if l<heapSize and A[l]>A[i]:
+        largest = l
+    if r<heapSize and A[r]>A[largest]:
+            largest = r
+    if largest != i:
+        A[i],A[largest]=A[largest],A[i]
+        maxHeapify(A,largest,heapSize)
+    print(A)
+
+def buildMaxHeap(A):
+    heapSize = len(A)
+    for i in range(int(len(A)/2)-1,-1,-1):
+        maxHeapify(A,i,heapSize)
+
+def heapSort(A):
+    buildMaxHeap(A)
+    heapSize=len(A)
+    print("$$$$$$$$$$$$$$$$$")
+    print(A)
+    print("$$$$$$$$$$$$$$$$$")
+    for i in range(len(A)-1,0,-1):
+        A[0],A[i]=A[i],A[0]
+        heapSize = heapSize - 1
+        maxHeapify(A,0,heapSize)
+
+
+A=[5,12,4,6,1,23,14]
+heapSort(A)
+print("################")
+print(A)
